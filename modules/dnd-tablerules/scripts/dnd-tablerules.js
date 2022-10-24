65e87201078a07e33249b6809604a968150d8998
Erik Itter <erik.itter@gmail.com>
2022-10-24 22:29:15 +0200

    small positive progress

--- modules/dnd-tablerules/scripts/dnd-tablerules.js
+++ modules/dnd-tablerules/scripts/dnd-tablerules.js
@@ -1,15 +1,47 @@
+function logOurs(level, message) {
+    let levelstring;
 
-/*
-    Long Rest no Heal
-*/
+    switch (level) {
+        case 0:
+            levelstring = "ERROR";
+            break;
+        case 1:
+            levelstring = "WARNING";
+            break;
+        case 2:
+            levelstring = "INFO";
+            break;
+        case 3:
+            levelstring = "DEBUG";
+            break;
+        default:
+            console.log("no logging level set");
+    }
 
-// prehook save current hp
-// posthook restore prior hp
+    console.log("Tablerules | " + levelstring + ":" + message);
+}
 
+function logError(message) {
+    logOurs(0, message);
+}
+
+function logWarning(message) {
+    logOurs(1, message);
+}
+
+function logInfo(message) {
+    logOurs(2, message);
+}
+
+function logDebug(message) {
+    logOurs(3, message);
+}
 
 /*
     Sticky Death Saves
 */
+
+// arguments[0].system.attributes.death
 
 // preupdate save current death saves
 // post update (find method) restore death saves to make the sticky
@@ -23,3 +55,17 @@ Hooks.on("init", function () {
 Hooks.on("ready", function () {
     console.log("Tablerules hooked onto ready.");
 });
+
+
+/*
+    Long Rest no Heal
+*/
+
+Hooks.on("dnd5e.preLongRest", function () {
+    logDebug("caught dnd5e.preLongRest hook.");
+    console.log(arguments);
+});
+
+
+// prehook save current hp
+// posthook restore prior hp
