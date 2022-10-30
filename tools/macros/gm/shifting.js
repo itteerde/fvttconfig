813e4a2c3c58cfde3b276a67aed7cccaef6115cf
Erik Itter <erik.itter@gmail.com>
2022-10-30 16:42:05 +0100

    adding temp hp automatically.

---
+++ tools/macros/gm/shifting.js
@@ -0,0 +1,13 @@
+const uuid = canvas.tokens.controlled[0].actor.uuid;
+const hasEffectApplied = await game.dfreds.effectInterface.hasEffectApplied("Shifting", uuid);
+
+if (canvas.tokens.controlled[0].actor.effects.find(e => e.label === "Shifting") === "undefined") {
+    //game.dfreds.effectInterface.toggleEffect("Shifting");
+    game.dfreds.effectInterface.addEffect("Shifting");
+}
+
+const tempHPRoll = await new Roll("@details.level + 1d6 + @abilities.con.mod", actor.getRollData()).toMessage();
+
+await actor.update({ "system.attributes.hp.temp": Math.max(Number(tempHPRoll.content), actor.system.attributes.hp.temp) });
+
+game.dfreds.effectInterface.toggleEffect("Shifting");
