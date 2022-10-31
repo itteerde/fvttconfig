3fef4f203bb323e22e5c3a2a6edbac5b24d618ed
Erik Itter <erik.itter@gmail.com>
2022-10-31 21:12:41 +0100

    cycles through art of wildcard tokens.

---
+++ tools/macros/gm/cycleWildcardToken.js
@@ -0,0 +1,7 @@
+// https://github.com/krbz999/zhell-macros/blob/main/tools/token/cycle_token_wildcards.js
+const currentImg = token.document.texture.src;
+const actorId = token.actor.id;
+const wildcardImages = await game.actors.get(actorId).getTokenImages();
+const currentIndex = wildcardImages.indexOf(currentImg);
+const next = (currentIndex + 1) === wildcardImages.length ? 0 : currentIndex + 1;
+await token.document.update({ "texture.src": wildcardImages[next] });
