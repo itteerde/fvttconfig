cce6064d0750d19a1e3cc889607f610ff54263c1
Erik Itter <erik.itter@gmail.com>
2022-10-31 02:19:07 +0100

    list active modules.

---
+++ tools/macros/dev/listModules.js
@@ -0,0 +1,4 @@
+// use in console F12
+game.modules.filter(m => m.active);
+// right-click on result, copy object
+// save in any kind of editor such as notepad++ (probably to big for pasting into Discord)
