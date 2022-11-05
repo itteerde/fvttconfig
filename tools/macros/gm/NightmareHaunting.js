75289852ac662892eeb7b76fdbc5adfaf9552ba5
Tyler Seabridge <taseabridge@gmail.com>
2022-11-05 17:55:31 -0400

    NightmareHaunting v1

--- tools/macros/gm/NightmareHaunting.js
+++ tools/macros/gm/NightmareHaunting.js
@@ -1,1 +1,34 @@
 // icon suggestion: icons/magic/death/undead-ghosts-trio-blue.webp
+const icon = icons/magic/death/hand-withered-gray.webp;
+const label = "Nightmare Haunting";
+const macroLabel = "Nightmare Haunting";
+
+
+if (canvas.tokens.controlled.length !== 1) {
+    ui.notifications.warn(`${macroLabel}, ${canvas.tokens.controlled.length} Actors selected. Select 1 and only 1.`, { permanent: true });
+    return;
+}
+
+if (actor.type !== "character") {
+    ui.notifications.warn(`${macroLabel}, Actor selected is type:${actor.type}. Select a character.`);
+    return;
+}
+
+const effect = actor.effects.find(e => e.getFlag("world", "Nightmare Haunting"));
+
+const keys = ["system.attributes.hp.max"];
+const effectData = { icon, label };
+
+  function updateValue(actorId, dir) {
+    const actor = game.actors.get(actorId);
+    const effect = actor.effects.find(e => foundry.utils.hasProperty(e, "flags.world.stress"));
+    const current = effect?.getFlag("world", "stress") ?? 0;
+    const stress = Number(Math.max(current + Number(dir), 0));
+    const changes = keys.map(key => {
+      return { key, mode: CONST.ACTIVE_EFFECT_MODES.ADD, value: `-${stress}` };
+    });
+    const effectData = { changes, icon, label };
+    foundry.utils.setProperty(effectData, "flags.world.stress", stress);
+    if (effect) return effect.update(effectData);
+    return actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+  }
