f42bbb6c591902cefc7445a14658cb842fb67e7f
Erik Itter <erik.itter@gmail.com>
2022-11-07 19:53:29 +0100

    laying out the idea.

--- tools/macros/gm/TwilightSanctuaryTakeTempHP.js
+++ tools/macros/gm/TwilightSanctuaryTakeTempHP.js
@@ -1,14 +1,16 @@
-// icon suggestion: icons/magic/light/light-lantern-lit-white.webp
-
 /* 
+    icon suggestion: icons/magic/light/light-lantern-lit-white.webp
+    source: https://github.com/itteerde/fvttconfig/blob/main/tools/macros/gm/TwilightSanctuaryTakeTempHP.js
+
     I think I like the version where the Cleric player gets the `Effect` (that can create the `Aura` [either already or once we adopt `Effect Macro` which we'll test]) for having the `Twilight Sanctuary` active, maybe being applied by hitting the `Feature` (think we have that possibility already). All players get another `Macro` `Take Twilight Sanctuary HP` that looks for the active `Twilight Sanctuary` (everyone can read everything through code, just not save changes non-locally, so everyone can find the source in order to read their Cleric-Level), `Roll` and upgrade their own `Actor.system.attributes.hp.temp`.
+
+    We do not let the Cleric player give the temp hp as that would require socket registering and programming or warpgate in order to modify an `Actor` the `User` does have the prvileges to modify.
 */
 
 const macroLabel = "Twilight Guidance tempHP";
 const sourceLabel = "Twilight Guidance";
 
-//get the source cleric
-
+//get the source Twilight Cleric
 const twilightClericPCs = game.actors.filter(a => {
     if (a.type !== "character")
         return;
@@ -42,4 +44,6 @@ if (distance > 30) {
     return;
 }
 
+// create the Roll
+// create the ChatMessage
 // upgrade hp.temp
