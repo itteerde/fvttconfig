f5f083fa573ea400a1d20c01a61aade569c00e73
Erik Itter <erik.itter@gmail.com>
2022-11-10 14:07:23 +0100

    working on it, this does not work yet.

--- tools/macros/gm/ShiftingBeasthide.js
+++ tools/macros/gm/ShiftingBeasthide.js
@@ -3,24 +3,11 @@ const icon = "icons/creatures/mammals/cat-hunched-glowing-red.webp";
 const label = "Shifting";
 const macroLabel = "Shifting";
 
-const keys = [
-    "system.bonuses.abilities.save"
+console.log(actor);
+await item.use();
+
+const changes = [
+    { key: "system.attributes.ac.bonus", value: "+1", mode: 2, priority: 20 }
 ];
-const effect = actor.effects.find(e => e.getFlag("world", "Shifting: Beasthide")); // do colon and whitespace work?
 
-const effectData = { icon, label };
-
-
-
-const uuid = canvas.tokens.controlled[0].actor.uuid;
-const hasEffectApplied = await game.dfreds.effectInterface.hasEffectApplied("Shifting", uuid);
-
-if (canvas.tokens.controlled[0].actor.effects.find(e => e.label === "Shifting") === "undefined") {
-    game.dfreds.effectInterface.addEffect("Shifting");
-}
-
-const tempHPRoll = await new Roll("@details.level + 1d6 + @abilities.con.mod", actor.getRollData()).toMessage();
-
-await actor.update({ "system.attributes.hp.temp": Math.max(Number(tempHPRoll.content), actor.system.attributes.hp.temp) });
-
-game.dfreds.effectInterface.toggleEffect("Shifting");
+const effectData = { changes, icon, label };
