a565c7393e18227e60676aa31420f41941504704
Tyler Seabridge <taseabridge@gmail.com>
2022-11-12 20:00:16 -0500

    ShiftingBeasthide 0.3

--- tools/macros/gm/ShiftingBeasthide.js
+++ tools/macros/gm/ShiftingBeasthide.js
@@ -4,9 +4,8 @@ const label = "Shifting";
 const macroLabel = "Shifting";
 
 console.log(actor);
-const itemUsePromise = await item.use();
-console.log({ itemUsePromise: itemUsePromise });
-
+await item.use();
+console.log(item);
 
 
 const changes = [
@@ -18,15 +17,18 @@ const effect = actor.effects.find(e => e.getFlag(Tablerules.dictionary.race.shif
 const current = effect?.getFlag(Tablerules.dictionary.race.shifter.features.shiftingBeasthide.scope, Tablerules.dictionary.race.shifter.features.shiftingBeasthide.key) ?? false;
 
 const effectData = { changes, icon, label, duration: { seconds: 60 } };
+foundry.utils.setProperty(effectData, Tablerules.dictionary.race.shifter.features.shiftingBeasthide.key, true);
 
 if (effect) { effect.update(effectData); }
 else {
     await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
 }
 
-await roll.toMessage({ speaker: ChatMessage.getSpeaker({ actor: cleric.name }), flavor: `getting temporary hit points by ${macroLabel} from ${cleric.name}.` });
+const roll = await new Roll(`1d6+${2 * actor.system.attributes.prof}`, actor.getRollData()).evaluate({ async: true })
 
-const currentTempHP = tokenTarget.actor.system.attributes.hp.temp !== null ? tokenTarget.actor.system.attributes.hp.temp : 0;
+await roll.toMessage({ speaker: ChatMessage.getSpeaker({ actor: actor.name }), flavor: `getting temporary hit points from ${Tablerules.dictionary.race.shifter.features.shiftingBeasthide.label}.` });
+
+const currentTempHP = actor.system.attributes.hp.temp !== null ? actor.system.attributes.hp.temp : 0;
 const newTempHP = Math.max(roll.total, currentTempHP);
 console.log({ message: "newTempHP", temp: newTempHP });
 
