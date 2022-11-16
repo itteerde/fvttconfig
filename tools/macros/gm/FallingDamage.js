17db29cee368a14cce4c530169c21f7168ca652d
Erik Itter <erik.itter@gmail.com>
2022-11-16 18:13:41 +0100

    planning Falling Damage.

---
+++ tools/macros/gm/FallingDamage.js
@@ -0,0 +1,11 @@
+/**
+ * Macro to be used from Control Bar by the DM selecting one and only one Actor the Falling Damage is to be applied to.
+ *
+ * [Issue](https://github.com/itteerde/fvttconfig/issues/43)
+ */
+
+// make sure one and only one Token is selected and it is Token5e
+// query GM for RollFormula
+// Roll damage
+// Requestor Save
+// Apply Damage and report in ChatMessage
