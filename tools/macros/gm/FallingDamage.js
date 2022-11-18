f4eab512c04fc2e10f3709de66f64d5a746793ef
Erik Itter <erik.itter@gmail.com>
2022-11-19 00:37:57 +0100

    setup only.

--- tools/macros/gm/FallingDamage.js
+++ tools/macros/gm/FallingDamage.js
@@ -4,8 +4,13 @@
  * [Issue](https://github.com/itteerde/fvttconfig/issues/43)
  */
 
+const macroLabel = "Fall Damage";
+const icon = "modules/Tablerules/icons/prototypes/falling01.webp";
+
 // make sure one and only one Token is selected and it is Token5e
 // query GM for RollFormula
 // Roll damage
 // Requestor Save
 // Apply Damage and report in ChatMessage
+
+console.log(`${macroLabel}: working on it.`);
