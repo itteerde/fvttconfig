03838c31df7ca596106e668501fb5748f429c9e4
Erik Itter <erik.itter@gmail.com>
2022-11-19 00:19:06 +0100

    working on lighting items. Values provided don't work, so tooling instead.

---
+++ tools/macros/dev/logWhatever.js
@@ -0,0 +1,3 @@
+const uuid = "Compendium.Tablerules.tablerules-actors.cTyD8CbZpHJ8aJqF";
+
+console.log(fromUuidSync(uuid)); // will get the item from the world, but returns an object if it is in a compendium)
