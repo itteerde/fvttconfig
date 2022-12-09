106e2bce4b7f1b809237b990c46bff1c3190f517
Erik Itter <erik.itter@gmail.com>
2022-12-09 21:18:00 +0100

    logging, removed development logging, damn.

--- tools/macros/item/feature/RegenerationSuppress.js
+++ tools/macros/item/feature/RegenerationSuppress.js
@@ -11,14 +11,11 @@ const icon = "icons/magic/life/heart-broken-red.webp";
 const tokensToBeModified = canvas.tokens.controlled;
 const actors = canvas.tokens.controlled.map(t => t.actor);
 
-console.log({ message: `${macroLabel}`, arguments: arguments, tokensToBeModified: tokensToBeModified, actors: actors });
-
 for (let i = 0; i < actors.length; i++) {
 
     const actor = actors[i];
 
     const effect = actor.effects.find(e => foundry.utils.hasProperty(e, flagKey));
-    console.log({ message: `${macroLabel}`, effect: effect });
 
     if (effect) {
         ui.notifications.warn(`${macroLabel}, found effect on Actor: ${actor.name}. Only one instance per Actor allowed.`, { permanent: false });
@@ -27,10 +24,8 @@ for (let i = 0; i < actors.length; i++) {
     const effectData = { icon, label: macroLabel, duration: { seconds: 6 } };
     foundry.utils.setProperty(effectData, flagKey, true);
     if (effect) {
-        console.log({ message: `${macroLabel}, updating effect.`, effect: effect, effectData: effectData });
         await effect.update(effectData);
     } else {
-        console.log({ message: `${macroLabel}, creating effect.`, effectData: effectData });
         await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
     }
 }
