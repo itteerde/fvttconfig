22e617ccf4b8f93cf4a7148d48c4e4c7eb91dcf2
Tyler Seabridge <taseabridge@gmail.com>
2022-12-10 22:07:34 -0500

    FaerieFireItemMacro

--- tools/macros/item/spell/FaerieFireItemMacro.js
+++ tools/macros/item/spell/FaerieFireItemMacro.js
@@ -12,7 +12,7 @@ if (await item.use() === null) {
 }
 
 
-templates = canvas.scene.templates.filter(t => t.flags.dnd5e !== undefined).filter(t => t.flags.dnd5e.origin === `Actor.${actor._id}.Item.${item._id}`);
+const templates = canvas.scene.templates.filter(t => t.flags.dnd5e !== undefined).filter(t => t.flags.dnd5e.origin === `Actor.${actor._id}.Item.${item._id}`);
 if (templates.length > 1) {
     ui.notifications.error(`${macroLabel}, ${templates.length} Measuring Templates (effects on map) found. Please delete them.`, { permanent: true });
     return;
@@ -22,25 +22,15 @@ const template = templates[0];
 
 console.log({ message: macroLabel, template: template });
 
-const tokensAffected = canvas.scene.tokens.filter(t => isTokenInTemplate(t, template));
+const templateCenterX = template.x + template.width * canvas.scene.grid.size / 5;
+const templateCenterY = template.y + template.width * canvas.scene.grid.size / 5 / 2;
 
-console.log({ message: macrolabel, tokensAffected: tokensAffected });
+console.log({ message: macroLabel, templateCenterX: templateCenterX, templateCenterY: templateCenterY });
 
-function isTokenInTemplate(token, template) {
+const tokensAffected = canvas.tokens.placeables.filter(p => (Math.abs(p.center.x - templateCenterX) / canvas.scene.grid.size * 5 < template.width && Math.abs(p.center.y - templateCenterY) / canvas.scene.grid.size * 5 < template.width));
+/*
 
-    console.log({ macroLabel: macroLabel, arguments: arguments, TokenCenter: token.center });
+*/
 
-    let templateCenterX = template.x + template.width / 2;
-    let templateCenterY = template.y + template.width / 2;
 
-    console.log({ message: `${macroLabel}`, templateCenter: { x: templateCenterX, y: templateCenterY } });
-
-    if (Math.abs(token.center.x - templateCenterX) > template.width) {
-        return false;
-    }
-    if (Math.abs(token.center.y - templateCenterY) > template.width) {
-        return false;
-    }
-
-    return true;
-};
+console.log({ message: macroLabel, tokensAffected: tokensAffected });
