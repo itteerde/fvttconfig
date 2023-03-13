b4da64adc90275bb86bc9d49c27477a24fbd5de5
Erik Itter <erik.itter@gmail.com>
2023-03-13 22:48:19 +0100

    Hide/Stealth #102

--- tools/macros/item/feature/Hide.js
+++ tools/macros/item/feature/Hide.js
@@ -2,9 +2,16 @@
  * Item-Macro Macro for the Hide Action
  */
 
-const use = await item.use();
-if (use === null) {
-    return;
-}
+await item.displayCard();
+const roll = new Roll("1d20+@skill", { skill: actor.getRollData().skills.ste.total, async: true });
+const chatMessage = await roll.toMessage({
+    speaker: ChatMessage.getSpeaker({ actor: actor }),
+    flavor: `${item.name}, stealth tobreak:`
+});
 
-console.log({ use: use });
+const stealth = chatMessage.rolls[0].total;
+
+const effectData = { icon: item.img, label: `Stealth: (${stealth})` };
+foundry.utils.setProperty(effectData, "flags.world.origin", item.uuid);
+foundry.utils.setProperty(effectData, "flags.core.statusId", `Stealth: (${stealth})`);
+await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
