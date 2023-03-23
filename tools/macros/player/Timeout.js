335a9517f27fdaea9acedc42f97943382e0048f9
Erik Itter <erik.itter@gmail.com>
2023-03-23 18:25:15 +0100

    probably not a solution even if the problem should exist

---
+++ tools/macros/player/Timeout.js
@@ -0,0 +1,8 @@
+ChatMessage.create({
+    content: `
+    <div style="width:100%;">
+        <img style="padding:5px; background-color: red;" src="icons/magic/time/clock-stopwatch-white-blue.webp"><b>Timeout?!</b><br/>
+        Should we take a moment pausing the game in order to clarify/ discuss?
+    </di>
+    `
+});
