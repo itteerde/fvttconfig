f38533ccb60d188a4daa5a2f52f49134cd69d18b
Erik Itter <erik.itter@gmail.com>
2023-06-21 14:27:19 +0200

    Hide with modifiers dialog and blind.

--- tools/macros/item/feature/Hide.js
+++ tools/macros/item/feature/Hide.js
@@ -5,16 +5,63 @@
 const showValue = false;
 
 await item.displayCard();
-const roll = new Roll("1d20+@skill", { skill: actor.getRollData().skills.ste.total, async: true });
+
+let modifier = "normal";
+
+const buttons = {
+    advantage: {
+        label: "Advantage",
+        callback: async (html) => {
+            modifier = "advantage";
+            return;
+        }
+    },
+    normal: {
+        label: "Normal",
+        callback: async (html) => {
+            modifier = "normal";
+            return;
+        }
+    },
+    disadvantage: {
+        label: "Disadvantage",
+        callback: async (html) => {
+            modifier = "disadvantage";
+            return;
+        }
+    }
+}
+
+await Dialog.wait({ title: item.name, buttons });
+
+let rollFormula = "1d20+@skill";
+
+if (modifier === "advantage") {
+    rollFormula = "2d20kh1+@skill";
+}
+
+if (modifier === "disadvantage") {
+    rollFormula = "2d20kl1+@skill";
+}
+
+const roll = await new Roll(rollFormula, { skill: actor.getRollData().skills.ste.total, async: true });
 const chatMessage = await roll.toMessage({
     speaker: ChatMessage.getSpeaker({ actor: actor }),
     flavor: `${item.name}, stealth tobreak:`
-});
+}, { rollMode: CONST.DICE_ROLL_MODES.BLIND });
 
 const stealth = chatMessage.rolls[0].total;
 
-const effectData = { icon: item.img, label: showValue ? `Stealth: (${stealth})` : "Stealth" };
-foundry.utils.setProperty(effectData, "flags.world.origin", item.uuid);
-foundry.utils.setProperty(effectData, "flags.core.statusId", showValue ? `Stealth: (${stealth})` : "Stealth");
-foundry.utils.setProperty(effectData, "flags.world.stealth", stealth);
-await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+let effect = actor.getEmbeddedCollection("ActiveEffect").find(e => e.label === "Stealth");
+
+if (effect === undefined) {
+    const effectData = { icon: item.img, label: showValue ? `Stealth: (${stealth})` : "Stealth" };
+    foundry.utils.setProperty(effectData, "flags.world.origin", item.uuid);
+    foundry.utils.setProperty(effectData, "flags.core.statusId", showValue ? `Stealth: (${stealth})` : "Stealth");
+    foundry.utils.setProperty(effectData, "flags.world.stealth", stealth);
+
+    await actor.createEmbeddedDocuments("ActiveEffect", [effectData]);
+} else {
+    await effect.update({ "flags.world.stealth": stealth });
+}
+
