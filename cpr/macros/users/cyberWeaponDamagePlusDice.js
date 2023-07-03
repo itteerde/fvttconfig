0f66bda18aa8476cc344243f132d5b12338b70d9
Tyler Seabridge <taseabridge@gmail.com>
2023-07-03 14:48:00 -0400

    0.0.9 test

---
+++ cpr/macros/users/cyberWeaponDamagePlusDice.js
@@ -0,0 +1,21 @@
+actor = canvas.tokens.controlled[0]?.actor;
+if (actor === undefined) {
+    ui.notifications.warn("Select exactly one Actor");
+    return;
+}
+
+weapons = actor.items.filter(i => i._id === "IZUsJJq5JGrZ8yUD");
+
+if (weapons.length !== 1) {
+    ui.notifications.warn("Exactly one weapon needs to be equiped for this to work.");
+    return;
+} else {
+    weapon = weapons[0];
+}
+
+damage = weapon.system.damage;
+numberOfDice = Number(damage.substr(0, damage.length - 2));
+
+ui.notifications.info(`Setting damage of ${actor.name}'s ${weapon.name} to ${numberOfDice + 1}d6`);
+
+await weapon.update({ "system.damage": `${numberOfDice + 1}d6` });
