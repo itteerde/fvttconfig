a2aabea8ace59e1d0eae25802cf5631e7ef273fb
Erik Itter <erik.itter@gmail.com>
2023-07-16 16:57:42 +0200

    issue #64

--- tools/macros/player/VolumeSave.js
+++ tools/macros/player/VolumeSave.js
@@ -1,3 +1,7 @@
+ui.notifications.info(`Saving volumes: globalPlaylistVolume=${game.settings.get('core', 'globalPlaylistVolume')}, globalAmbientVolume=${game.settings.get('core', 'globalAmbientVolume')}, globalInterfaceVolume=${game.settings.get('core', 'globalInterfaceVolume')}`, { permanent: true });
+ui.notifications.info("Volume values are on a logarithmic scale within a range of 0-1. Don't be alarmed by low values.",
+    { permanent: true })
+
 game.user.update({
     'flags.world': {
         globalPlaylistVolume: game.settings.get('core', 'globalPlaylistVolume'),
