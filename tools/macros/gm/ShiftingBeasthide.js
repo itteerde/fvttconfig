86a17486f35b12c931e02f525773fb431999f579
Erik Itter <erik.itter@gmail.com>
2023-07-26 14:39:22 +0200

    issue #149

--- tools/macros/gm/ShiftingBeasthide.js
+++ tools/macros/gm/ShiftingBeasthide.js
@@ -1,23 +1,17 @@
-// icon suggestion: icons/creatures/mammals/cat-hunched-glowing-red.webp
-const icon = "icons/creatures/mammals/cat-hunched-glowing-red.webp";
-const label = "Shifting";
-const macroLabel = "Shifting";
+const icon = item.img;
+const label = item.name;
 
 if ((await item.use()) === null) {
+    ui.notifications.warn(`${label} has no uses left.`);
     return;
 }
-
 
 const changes = [
     { key: "system.attributes.ac.bonus", value: "+1", mode: 2, priority: 20 }
 ];
 
-const effect = actor.effects.find(e => e.getFlag(Tablerules.dictionary.race.shifter.features.shiftingBeasthide.scope, Tablerules.dictionary.race.shifter.features.shiftingBeasthide.key));
-
-const current = effect?.getFlag(Tablerules.dictionary.race.shifter.features.shiftingBeasthide.scope, Tablerules.dictionary.race.shifter.features.shiftingBeasthide.key) ?? false;
-
+const effect = actor.effects.find(e => e.name === label);
 const effectData = { changes, icon, label, duration: { seconds: 60 } };
-foundry.utils.setProperty(effectData, `flags.${Tablerules.dictionary.race.shifter.features.shiftingBeasthide.scope}.${Tablerules.dictionary.race.shifter.features.shiftingBeasthide.key}`, true);
 
 if (effect) { effect.update(effectData); }
 else {
@@ -26,7 +20,7 @@ else {
 
 const roll = await new Roll(`1d6+${2 * actor.system.attributes.prof}`, actor.getRollData()).evaluate({ async: true })
 
-await roll.toMessage({ speaker: ChatMessage.getSpeaker({ actor: actor.name }), flavor: `getting temporary hit points from ${Tablerules.dictionary.race.shifter.features.shiftingBeasthide.label}.` });
+await roll.toMessage({ speaker: ChatMessage.getSpeaker({ actor: actor.name }), flavor: `getting temporary hit points from ${label}.` });
 
 const currentTempHP = actor.system.attributes.hp.temp !== null ? actor.system.attributes.hp.temp : 0;
 const newTempHP = Math.max(roll.total, currentTempHP);
