8548cbf0d8e6ef56c7246ecd05661e1fc8c50721
Erik Itter <erik.itter@gmail.com>
2023-08-15 21:02:45 +0200

    ...

--- tools/macros/dev/snippletsErik.js
+++ tools/macros/dev/snippletsErik.js
@@ -5,7 +5,7 @@ game.actors.getName(game.user.targets.find(t => true).actor.name).items.filter(s
 game.actors.get("w0H5GHtw7ser0ntj").items.filter(s => s.type === "spell").map(e => { return { name: e.name, level: e.system.level } }).sort((a, b) => { if (a.level !== b.level) { return a.level - b.level } else { a.name.localeCompare(b.name) } })
 
 // Actor's spells sorted by level and within in alpabetical order
-game.actors.get("w0H5GHtw7ser0ntj").items.filter(
+game.actors.get("kf5lTiiNlXd1B9vq").items.filter(
     s => s.type === "spell").map(
         e => {
             return {
