a5abb6e92255927966a6add2556c831d5159ad8a
Erik Itter <erik.itter@gmail.com>
2024-08-17 22:09:09 +0200

    issue #253 Hit Dice Regain

---
+++ tools/macros/gm/HitDiceRecovery.js
@@ -0,0 +1,28 @@
+async function regainHitDice(actor) {
+    const classes = actor.itemTypes.class.toSorted((a, b) => {
+        return parseInt(b.system.hitDice.substring(1)) - parseInt(a.system.hitDice.substring(1));
+    });
+
+    let hdUsed = classes.reduce((acc, c) => acc + c.system.hitDiceUsed, 0);
+    let hdRegained = 0;
+    let levels = classes.reduce((acc, c) => acc + c.system.levels, 0);
+
+    console.log({ actor: actor, levels: levels, hdUsed: hdUsed, hdRegained: hdRegained });
+
+    while (hdRegained < Math.ceil(levels / 2) && hdRegained < hdUsed) {
+
+        for (let i = 0; i < classes.length; i++) {
+            if (classes[i].system.hitDiceUsed > 0) {
+                await classes[i].update({ "system.hitDiceUsed": classes[i].system.hitDiceUsed - 1 });
+                hdRegained++;
+                break;
+            }
+        }
+    }
+
+}
+
+canvas.tokens.controlled.forEach(t => {
+    console.log({ t: t, actor: actor });
+    regainHitDice(t.actor);
+})
