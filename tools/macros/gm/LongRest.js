5d80d53fc7c02ebf480f9c02387da3213159b381
Erik Itter <erik.itter@gmail.com>
2024-08-17 20:11:30 +0200

    issue #252 Long Rest

--- tools/macros/gm/LongRest.js
+++ tools/macros/gm/LongRest.js
@@ -1,6 +1,8 @@
-let players = game.folders.find(f => f.name === "PCs").contents.filter(a => a.type === "character").forEach(pc => {
+//let players = game.folders.find(f => f.name === "PCs").contents.filter(a => a.type === "character").forEach(pc => {
 
-    let item = pc.items.find(i => i.name === "Long Rest")
+canvas.tokens.controlled.forEach(t => {
+
+    t.actor.item = pc.items.find(i => i.name === "Long Rest")
     if (item) {
         item.update({ "system.uses.value": Math.min(item.system.uses.max, item.system.uses.value + 1) });
     } else {
