39e06b177bac01cc778cee0424018165ab2db3a9
Erik Itter <erik.itter@gmail.com>
2024-08-17 20:19:55 +0200

    issue #252 Long Rest

---
+++ tools/macros/gm/LongRestAll.js
@@ -0,0 +1,10 @@
+game.folders.find(f => f.name === "PCs").contents.filter(a => a.type === "character").forEach(pc => {
+
+    let item = pc.items.find(i => i.name === "Long Rest")
+    if (item) {
+        item.update({ "system.uses.value": Math.min(item.system.uses.max, item.system.uses.value + 1) });
+    } else {
+        ui.notifications.warn(`${pc.name}: no Long Rest Item found`, { permanent: true });
+    }
+
+});;
