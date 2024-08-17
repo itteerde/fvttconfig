186dc3f2ecad2924c488f5dcb1bdce2f772ec9ed
Erik Itter <erik.itter@gmail.com>
2024-08-17 20:16:31 +0200

    issue #252 Long Rest

--- tools/macros/gm/LongRest.js
+++ tools/macros/gm/LongRest.js
@@ -2,11 +2,11 @@
 
 canvas.tokens.controlled.forEach(t => {
 
-    t.actor.item = pc.items.find(i => i.name === "Long Rest")
+    let item = t.actor.items.find(i => i.name === "Long Rest")
     if (item) {
         item.update({ "system.uses.value": Math.min(item.system.uses.max, item.system.uses.value + 1) });
     } else {
-        ui.notifications.warn(`${pc.name}: no Long Rest Item found`, { permanent: true });
+        ui.notifications.warn(`${t.name}: no Long Rest Item found`, { permanent: true });
     }
 
 });;
