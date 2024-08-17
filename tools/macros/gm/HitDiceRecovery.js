04d8fdbeb1bcf3cadc5394305b965cf55081908f
Erik Itter <erik.itter@gmail.com>
2024-08-17 22:36:21 +0200

    issue #253 Hit Dice Regain

--- tools/macros/gm/HitDiceRecovery.js
+++ tools/macros/gm/HitDiceRecovery.js
@@ -24,4 +24,12 @@ async function regainHitDice(actor) {
 
 canvas.tokens.controlled.forEach(t => {
     regainHitDice(t.actor);
-})
+
+    let item = t.actor.items.find(i => i.name === "Long Rest")
+    if (item) {
+        item.update({ "system.uses.value": Math.min(item.system.uses.max, item.system.uses.value + 1) });
+    } else {
+        ui.notifications.warn(`${t.name}: no Long Rest Item found`, { permanent: true });
+    }
+});
+
--- tools/macros/gm/HitDiceRecoveryAll.js
+++ tools/macros/gm/HitDiceRecoveryAll.js
@@ -23,5 +23,14 @@ async function regainHitDice(actor) {
 }
 
 game.folders.find(f => f.name === "PCs").contents.filter(pc => pc.type === "character").forEach(a => {
+
     regainHitDice(a);
-})
+
+    let item = pc.items.find(i => i.name === "Long Rest")
+    if (item) {
+        item.update({ "system.uses.value": Math.min(item.system.uses.max, item.system.uses.value + 1) });
+    } else {
+        ui.notifications.warn(`${pc.name}: no Long Rest Item found`, { permanent: true });
+    }
+});
+
